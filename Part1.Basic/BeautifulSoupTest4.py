dbc47ef6c7258db0de79a13b4d2fa25876900ad7
qqmzqq <chen072262@gmail.com>
2017-09-11 18:38:58 +0800

    BeautifulSoap4.py

--- Part1.Basic/BeautifulSoupTest4.py
+++ Part1.Basic/BeautifulSoupTest4.py
@@ -6,5 +6,5 @@ from bs4 import BeautifulSoup
 html = urlopen("http://www.pythonscraping.com/pages/page3.html")
 bsObj = BeautifulSoup(html,"html.parser")
 
-for child in bsObj.find("table",{"id","giftlist"}).children:
+for child in bsObj.find("table",{"id":"giftList"}).children:
     print(child)
